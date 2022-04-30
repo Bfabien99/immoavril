21715860e2b2784f21a04088607c885d83916f8b
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-04-30 14:27:42 +0000

    db update

--- xmobilier.sql
+++ xmobilier.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 29, 2022 at 02:38 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Hôte : localhost
+-- Généré le : sam. 30 avr. 2022 à 16:26
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `xmobilier`
+-- Base de données : `xmobilier`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Structure de la table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -38,7 +38,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `admin`
+-- Déchargement des données de la table `admin`
 --
 
 INSERT INTO `admin` (`admin_id`, `ad_nom`, `ad_pseudo`, `ad_contact`, `ad_email`, `ad_password`, `ad_created`) VALUES
@@ -47,33 +47,32 @@ INSERT INTO `admin` (`admin_id`, `ad_nom`, `ad_pseudo`, `ad_contact`, `ad_email`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Structure de la table `customers`
 --
 
 CREATE TABLE `customers` (
   `cust_id` int(11) NOT NULL,
-  `cust_nom` varchar(100) NOT NULL,
-  `cust_prenoms` varchar(100) NOT NULL,
-  `cust_pseudo` varchar(100) NOT NULL,
-  `cust_contact` varchar(20) NOT NULL,
+  `cust_nom` varchar(100) DEFAULT NULL,
+  `cust_prenoms` varchar(100) DEFAULT NULL,
+  `cust_pseudo` varchar(100) DEFAULT NULL,
+  `cust_contact` varchar(20) DEFAULT NULL,
   `cust_email` varchar(100) DEFAULT NULL,
-  `cust_password` text NOT NULL,
+  `cust_password` text DEFAULT NULL,
   `cust_photo` text DEFAULT NULL,
   `cust_created` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `customers`
+-- Déchargement des données de la table `customers`
 --
 
 INSERT INTO `customers` (`cust_id`, `cust_nom`, `cust_prenoms`, `cust_pseudo`, `cust_contact`, `cust_email`, `cust_password`, `cust_photo`, `cust_created`) VALUES
-(1, 'brou', 'kouadio fabien', 'fabien', '0152148864', 'fabienbrou99@gmail.com', 'b2dd7d8e6f1722a1e23a101e890c77f0', NULL, '2022-04-28 17:51:28'),
-(2, 'sit voluptate sed', 'officia iste anim au', 'dolor ut eligendi ex', '', 'tyfinywa@mailinator.com', '3bf7f9ce3190e8b37595e7c8133cc090', NULL, '2022-04-28 19:25:10');
+(1, 'brou', 'k', 'fabie', '01521488', 'fabienbrou99@gmail.com', '8082b382bea4c85367617e7271768276', '$2y$10$ereemEkQdNtARgMkKYOg5jTia2Wlp593RhGItauPuCYlqSD.2Gpexels-jack-winbow-1559486.jpg', '2022-04-28 17:51:28');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `proprietes`
+-- Structure de la table `proprietes`
 --
 
 CREATE TABLE `proprietes` (
@@ -91,6 +90,7 @@ CREATE TABLE `proprietes` (
   `image` text NOT NULL,
   `nom_proprio` varchar(100) NOT NULL,
   `contact_proprio` varchar(20) NOT NULL,
+  `email_proprio` varchar(100) DEFAULT NULL,
   `customer_id` int(11) DEFAULT NULL,
   `vue` int(11) NOT NULL DEFAULT 0,
   `prop_created` datetime DEFAULT current_timestamp(),
@@ -98,25 +98,25 @@ CREATE TABLE `proprietes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `proprietes`
+-- Déchargement des données de la table `proprietes`
 --
 
-INSERT INTO `proprietes` (`prop_id`, `titre`, `nb_piece`, `nb_chambre`, `nb_douche`, `nb_wc`, `addresse`, `superficie`, `type`, `prix`, `Description`, `image`, `nom_proprio`, `contact_proprio`, `customer_id`, `vue`, `prop_created`, `enable`) VALUES
-(1, 'Grande Maison Bleue', 3, 1, 1, 2, 'earum quis doloremqu', 2000, 'location', 760000, 'dolorem temporibus b', '$2y$10$w0f0LZgDNlefZCAvynYOipD6vbvqxLwaDgqPZeTp1QxU.qJX7C.lucia-macedo-z9FST1uGqlc-unsplash.jpg', 'eius tempora perfere', '+1 (644) 721-7192', NULL, 0, '2022-04-28 22:35:04', 1),
-(2, 'Petite Prairie', 18, 12, 11, 12, 'eos commodo aut vero', 60, 'location', 99999000, 'voluptatum dolore ra', '$2y$10$MOfFPxOCCyimR5z2FhPgOxa0pFyGW9TWFNh.r6lBoiShKMuGmcR6DJI_0026.jpg', 'dolorem assumenda es', '+1 (848) 295-5903', NULL, 3, '2022-04-28 22:43:55', 1);
+INSERT INTO `proprietes` (`prop_id`, `titre`, `nb_piece`, `nb_chambre`, `nb_douche`, `nb_wc`, `addresse`, `superficie`, `type`, `prix`, `Description`, `image`, `nom_proprio`, `contact_proprio`, `email_proprio`, `customer_id`, `vue`, `prop_created`, `enable`) VALUES
+(5, 'natus alias quis totam et cum.', 3, 2, 3, 4, 'aéroport félix houphouët boigny, abidjan, côte d\'ivoire', 127, 'vendre', 634, 'zczc', '$2y$10$gIdiCkuzwnO1kXNamK0TH.SI14WdcKRRI9UqQyquXt7tiDEDZdFKpexels-chloe-1043471.jpg', 'sonny heller', '0297488435', 'your.email+fakedata67496@gmail.com', NULL, 1, '2022-04-30 10:24:17', 1),
+(6, 'laborum eaque reprehenderit est sed', 441, 593, 386, 421, '975 hartmann corners', 597, 'location', 195, 'hic qui cumque non et vitae voluptas. nemo eius saepe omnis. provident eaque a sequi architecto exercitationem illo molestiae. provident eum eius eveniet ab dignissimos dolores.', '$2y$10$PBANXAJVCm.sR9XCo58lORf.L5miUE34yLSmI9YeMXb0qQhgUSlapexels-photomix-company-101808.jpg', 'eileen prosacco', '2896064000', 'your.email+fakedata97405@gmail.com', NULL, 0, '2022-04-30 10:27:16', 0);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `admin`
+-- Index pour la table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indexes for table `customers`
+-- Index pour la table `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`cust_id`),
@@ -124,33 +124,44 @@ ALTER TABLE `customers`
   ADD UNIQUE KEY `cust_email` (`cust_email`);
 
 --
--- Indexes for table `proprietes`
+-- Index pour la table `proprietes`
 --
 ALTER TABLE `proprietes`
   ADD PRIMARY KEY (`prop_id`),
-  ADD UNIQUE KEY `addresse` (`addresse`);
+  ADD UNIQUE KEY `addresse` (`addresse`),
+  ADD KEY `customer_id` (`customer_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT pour la table `customers`
 --
 ALTER TABLE `customers`
   MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `proprietes`
+-- AUTO_INCREMENT pour la table `proprietes`
 --
 ALTER TABLE `proprietes`
-  MODIFY `prop_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `prop_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `proprietes`
+--
+ALTER TABLE `proprietes`
+  ADD CONSTRAINT `proprietes_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`cust_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
